07246e2b1334769bb7690bc01741360681d4af11
ecru <woovoodoor@yahoo.co.jp>
2020-05-10 23:55:05 +0900

    StaticPagesルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
-  get 'staticpages/about'
+  get 'about' => 'static_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
