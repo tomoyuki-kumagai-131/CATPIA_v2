3d83e1b403c43993ad4731ff38882546d841ae66
ecru <woovoodoor@yahoo.co.jp>
2020-05-12 19:55:25 +0900

    full_titleメソッドを定義

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = 'CATPIA'
+    if page_title.blank?
+      base_title
+    else
+      "#{page_title} - #{base_title}"
+    end
+  end
 end
