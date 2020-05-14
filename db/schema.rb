6dc837e9e3fd8cd6a93d748ca22ec65549c37e36
ecru <woovoodoor@yahoo.co.jp>
2020-05-15 01:37:59 +0900

    usesテーブルにpassword_digest追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_14_155239) do
+ActiveRecord::Schema.define(version: 2020_05_14_163305) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_05_14_155239) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
