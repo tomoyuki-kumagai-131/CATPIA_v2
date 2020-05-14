1193e8493173e2b38e1756587f360e4650f28d73
ecru <woovoodoor@yahoo.co.jp>
2020-05-15 01:44:01 +0900

    Rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
+has_secure_password
+validates :password, presence: true, length: { minimum: 6 }
+
   private
 
     def downcase_email
