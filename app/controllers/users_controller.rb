47fa6391de9694cbd0d902213643b73a7175f2d0
ecru <woovoodoor@yahoo.co.jp>
2020-05-14 21:28:52 +0900

    usersコントローラー作成・routes.rb:修正

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'signup' => 'users#new'
+  resources :users
   root 'static_pages#home'
   get 'about' => 'static_pages#about'
   
