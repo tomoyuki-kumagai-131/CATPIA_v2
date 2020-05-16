249cf1873b17afd5c78edb447e7b6ca096fe604b
ecru <woovoodoor@yahoo.co.jp>
2020-05-17 06:33:04 +0900

    アクション追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+ 
   root 'static_pages#home'
   get 'about' => 'static_pages#about'
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
 end
