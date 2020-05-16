ef17e9db18e7332eb09d04b5c10f345014e7752c
ecru <woovoodoor@yahoo.co.jp>
2020-05-16 14:17:58 +0900

    users__signup_spec 作成

--- spec/requests/users_signup_spec.rb
+++ spec/requests/users_signup_spec.rb
@@ -9,4 +9,25 @@ RSpec.describe "ユーザー登録", type: :request do
     expect(response).to be_success
     expect(response).to have_http_status "200"
   end
+
+  it "有効なユーザーで登録" do
+    expect {
+      post users_path, params: { user: { name: "Example User",
+                                         email: "user@example.com",
+                                         password: "password",
+                                         password_confirmation: "password" } }
+    }.to change(User, :count).by(1)
+    redirect_to @user
+    follow_redirect!
+    expect(response).to render_template('users/show')
+  end
+
+  it "無効なユーザーで登録" do
+    expect {
+      post users_path, params: { user: { name: "",
+                                         email: "user@example.com",
+                                         password: "password",
+                                         password_confirmation: "pass" } }
+    }.not_to change(User, :count)
+  end
 end
