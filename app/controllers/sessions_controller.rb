157f0f864dfac96a76751f37c4baeefb222063dd
ecru <woovoodoor@yahoo.co.jp>
2020-05-18 07:42:16 +0900

    RSpecテスト成功：ログイン（リクエスト）

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,9 +12,9 @@ class SessionsController < ApplicationController
       render 'new'
     end
   end
-  
+
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,7 +13,7 @@ module SessionsHelper
     !current_user.nil?
   end
 
-  def log_out?
+  def log_out
     session.delete(:user_id)
     @current_user = nil
   end
