2dd06c9670579c2036ecd67fbc0eaff80265a05c
ecru <woovoodoor@yahoo.co.jp>
2020-05-18 06:18:16 +0900

    内容修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 //= require bootstrap
 //= require activestorage
 //= require turbolinks
