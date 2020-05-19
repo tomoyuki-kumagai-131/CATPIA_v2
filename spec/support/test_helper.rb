d2dc48d81edca433be249e23d19cd3bb19b1756c
ecru <woovoodoor@yahoo.co.jp>
2020-05-19 12:23:14 +0900

    RSpecテスト成功:Remember me機能等

--- spec/support/test_helper.rb
+++ spec/support/test_helper.rb
@@ -1,5 +1,28 @@
-include ApplicationHelper
+include ApplicationHelper #full_titleメソッドを読み込む
 
 def is_logged_in?
   !session[:user_id].nil?
+end
+
+def login_for_request(user)
+  post login_path, params: { session: { email: user.email,
+                                        password: user.password } }
+end
+
+def login_remember(user)
+  post login_path, params: { session: { email: user.email,
+                                        password: user.password,
+                                        remember_me: '1' } }
+end
+
+def current_user
+  if (user_id = session[:user_id])
+    User.find_by(id: user_id)
+  elsif (user_id = cookies.signed[:user_id])
+    user = User.find_by(id: user_id)
+    if user && user.authenticated?(cookies[:remember_token])
+      login_for_request user
+      user
+    end
+  end
 end
