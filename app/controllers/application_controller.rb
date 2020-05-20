64d29f08a1b2a0b8af5605bf6a4474c651851ff8
ecru <woovoodoor@yahoo.co.jp>
2020-05-20 14:07:49 +0900

    ログイン有無を確認するメソッドの定義

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  private
+    # ログイン済ユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "ログインしてください"
+        redirect_to login_url
+      end
+    end
 end
