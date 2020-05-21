729871727477b8bd0c22b8f29bd9865f79d415fd
ecru <woovoodoor@yahoo.co.jp>
2020-05-21 09:54:14 +0900

    store_location追記

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user) #チェックがついている時限定でRemember me有効化
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:danger] = 'メールアドレスとパスワードの組み合わせが誤っています'
       render 'new'
