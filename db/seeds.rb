ad880d886bd79b697b6472b04ca9583864fba135
ecru <woovoodoor@yahoo.co.jp>
2020-05-22 00:07:40 +0900

    Fakerによりユーザーデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,17 @@
+ User.create!(name: "山田 太郎",
+              email: "sample@example.com",
+              password:              "foobar",
+              password_confirmation: "foobar" )
+99.times do |n|
+  name  = Faker::Name.name
+  email = "sample-#{n+1}@example.com"
+  password = "password"
+  User.create!(name:  name,
+              email:  email,
+              password:              password,
+              password_confirmation: password)
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
