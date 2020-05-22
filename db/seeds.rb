77cca5547f243ca1c3cec0da2d9ad517cf27f974
ecru <woovoodoor@yahoo.co.jp>
2020-05-22 19:16:50 +0900

    1人目ユーザー:admin: true追記

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
  User.create!(name: "山田 太郎",
               email: "sample@example.com",
               password:              "foobar",
-              password_confirmation: "foobar" )
+              password_confirmation: "foobar",
+              admin: true)
 99.times do |n|
   name  = Faker::Name.name
   email = "sample-#{n+1}@example.com"
