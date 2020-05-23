a64cac640b30399556198ab0a0ad6b8f249e1e6d
ecru <woovoodoor@yahoo.co.jp>
2020-05-24 01:29:02 +0900

    ファクトリ定義修正

--- spec/factories/shops.rb
+++ spec/factories/shops.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :shop do
-    name { Faker::Cafe.shop }
+    name { "ひとやすみ。" }
     address { "名古屋市中区栄１−９−３" }
     web_page { "https://ameblo.jp/nekocafe-hitoyasumi/" }
-    description { "閑静な住宅街にある猫カフェ。ついつい通ってしまいます。"　}
-    recommended_points { "世界の珍しい猫が集まっっています！"　}
+    description { "閑静な住宅街にある猫カフェ。ついつい通ってしまいます。" }
+    recommended_points { "世界の珍しい猫が集まっています！" }
     rating { 5 }
     association :user
   end
