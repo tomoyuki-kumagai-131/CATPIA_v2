2a9d3beb5894cecb1fed8752bf94d9cbdd558cf8
ecru <woovoodoor@yahoo.co.jp>
2020-05-24 00:02:54 +0900

    バリデーション追記

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,15 @@
 class Shop < ApplicationRecord
   belongs_to :user
+  validates :user_id, presence: true
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :description, length: { maximum: 150 }
+  validates :address, presence: true, length: { maximum: 50 }
+  validates :recommended_points, length: { maximum: 50 }
+  validates :rating,
+            :numericality => {
+              :only_interger => true,
+              :greater_than_or_equal_to => 1,
+              :less_than_or_equal_to => 5
+            },
+            allow_nil: true
 end
