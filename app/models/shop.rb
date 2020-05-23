ed3df5a6ccb3e969ec3c62475308c8fca9ee5842
ecru <woovoodoor@yahoo.co.jp>
2020-05-24 07:59:34 +0900

    投稿を降順表示させる

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,5 +1,7 @@
 class Shop < ApplicationRecord
+  has_many :shops, dependent: :destroy
   belongs_to :user
+  default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :name, presence: true, length: { maximum: 30 }
   validates :description, length: { maximum: 150 }
