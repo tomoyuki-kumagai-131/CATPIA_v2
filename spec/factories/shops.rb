4d3ba4100735ad06379d920ba81084464673179f
ecru <woovoodoor@yahoo.co.jp>
2020-05-24 00:05:43 +0900

    RSpecテスト用ファクトリ作成

--- spec/factories/shops.rb
+++ spec/factories/shops.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :shop do
-    name "MyString"
-    address "MyString"
-    web_page "MyString"
-    description "MyText"
-    recommended_points "MyText"
-    rating 1
-    user nil
+    name { Faker::Cafe.shop }
+    address { "名古屋市中区栄１−９−３" }
+    web_page { "https://ameblo.jp/nekocafe-hitoyasumi/" }
+    description { "閑静な住宅街にある猫カフェ。ついつい通ってしまいます。"　}
+    recommended_points { "世界の珍しい猫が集まっっています！"　}
+    rating { 5 }
+    association :user
   end
 end
