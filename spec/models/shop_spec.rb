315b7a6030b70c9fb4cb92c79fb78b5d478bb2f1
ecru <woovoodoor@yahoo.co.jp>
2020-05-24 01:30:33 +0900

    RSpecテスト:バリデーション(モデル)

--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Shop, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:shop) { create(:shop) }
+
+  context "バリデーション" do
+    it "有効な状態であること" do
+      expect(shop).to be_valid
+    end
+
+    it "店名がなければ無効な状態であること" do
+      shop = build(:shop, name: nil)
+      shop.valid?
+      expect(shop.errors[:name]).to include("を入力してください")
+    end
+
+    it "店名が30文字以内であること" do
+      shop = build(:shop, name: "あ" *31)
+      shop.valid?
+      expect(shop.errors[:name]).to include("は30文字以内で入力してください")
+    end
+
+    it "説明が150文字以内であること" do
+      shop = build(:shop, description: "あ" * 151)
+      shop.valid?
+      expect(shop.errors[:description]).to include("は150文字以内で入力してください")
+    end
+
+    it "おすすめポイントが50文字以内であること" do
+      shop = build(:shop, recommended_points: "あ" * 51)
+      shop.valid?
+      expect(shop.errors[:recommended_points]).to include("は50文字以内で入力してください")
+    end
+
+    it "ユーザーIDがなければ無効な状態であること" do
+      shop = build(:shop, user_id: nil)
+      shop.valid?
+      expect(shop.errors[:user_id]).to include("を入力してください")
+    end
+
+    it "人気度が1以上でなければ無効な状態であること" do
+      shop = build(:shop, rating: 0)
+      shop.valid?
+      expect(shop.errors[:rating]).to include("は1以上の値にしてください")
+    end
+
+    it "人気度が5以下でなければ無効な状態であること" do
+      shop = build(:shop, rating: 6)
+      shop.valid?
+      expect(shop.errors[:rating]).to include("は5以下の値にしてください")
+    end
+  end
 end
