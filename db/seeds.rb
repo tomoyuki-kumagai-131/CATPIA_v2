e79abd1188c3673bd53967266381010a5ddeeae5
ecru <woovoodoor@yahoo.co.jp>
2020-05-24 19:29:02 +0900

    10個分の猫カフェ投稿データを作成することについて追記

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,27 @@
- User.create!(name: "山田 太郎",
-              email: "sample@example.com",
-              password:              "foobar",
-              password_confirmation: "foobar",
-              admin: true)
+User.create!(name: "山田 太郎",
+            email: "sample@example.com",
+            password:              "foobar",
+            password_confirmation: "foobar",
+            admin: true)
+
 99.times do |n|
   name  = Faker::Name.name
   email = "sample-#{n+1}@example.com"
   password = "password"
   User.create!(name:  name,
-              email:  email,
-              password:              password,
-              password_confirmation: password)
+               email:  email,
+               password: password,
+               password_confirmation: password)
 end
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |n|
+  Shop.create!(name: Faker::Company.name,
+               address: "名古屋市中区栄１−９−３",
+               web_page: "https://ameblo.jp/nekocafe-hitoyasumi/",
+               description: "閑静な住宅街にある猫カフェ。ついつい通ってしまいます。",
+               recommended_points: "世界の珍しい猫が集まっています！",
+               rating: 5,
+               user_id: 1)
+end
+
+
