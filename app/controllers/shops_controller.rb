8a101bc8d23340f51c3d519ff14a7fc632e6509c
ecru <woovoodoor@yahoo.co.jp>
2020-05-26 12:38:35 +0900

    createアクションの追記

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -5,4 +5,21 @@ class ShopsController < ApplicationController
   def new
     @shop = Shop.new
   end
+
+  def create
+    @shop = current_user.shops.build(shop_params)
+    if @shop.save
+      flash[:success] = "ねこカフェの投稿が完了しました！"
+      redirect_to root_url
+  
+    else
+      render 'shops/new'
+  end
+end
+
+  private
+
+    def shop_params
+      params.require(:shop).permit(:name, :description, :address, :recommended_points, :web_page, :rating)
+    end
 end
