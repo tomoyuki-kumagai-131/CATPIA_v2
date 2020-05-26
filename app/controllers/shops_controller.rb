d18f1eeffb548b191ce196c9255f3f199b04d160
ecru <woovoodoor@yahoo.co.jp>
2020-05-26 16:09:59 +0900

    createアクション追記・shop_params追記

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -10,15 +10,18 @@ class ShopsController < ApplicationController
     @shop = current_user.shops.build(shop_params)
     if @shop.save
       flash[:success] = "ねこカフェの投稿が完了しました！"
-      redirect_to root_url
-  
+      redirect_to shop_path(@shop) # 投稿後、投稿詳細画面へ遷移
     else
       render 'shops/new'
+  end
+
+  def show
+    @shop = Shop.find(params[:id])
   end
 end
 
   private
-
+    # 登録できる項目をshop_paramsメソッドで定義する
     def shop_params
       params.require(:shop).permit(:name, :description, :address, :recommended_points, :web_page, :rating)
     end
