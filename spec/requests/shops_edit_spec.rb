595267e57c02751d310310ef7a59a169849f3d0a
ecru <woovoodoor@yahoo.co.jp>
2020-06-01 17:13:16 +0900

    RSpec:ねこカフェ投稿編集：画像投稿追加(リクエスト)

--- spec/requests/shops_edit_spec.rb
+++ spec/requests/shops_edit_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe "ねこカフェ編集", type: :request do
   let!(:user) { create(:user) }
   let!(:other_user) { create(:user) }
   let!(:shop) { create(:shop, user: user) }
+  let(:picture2_path) { File.join(Rails.root, 'spec/fixtures/test-shop2.jpg') }  # 追記
+  let(:picture2) { Rack::Test::UploadedFile.new(picture2_path) }  # 追記
 
   context "認可されたユーザーの場合" do
     it "レスポンスが正常に表示されること・フレンドリーフォワーディング" do
@@ -15,7 +17,8 @@ RSpec.describe "ねこカフェ編集", type: :request do
                                               address: "名古屋市中区栄１−１−１",
                                               recommended_points: "黒猫の「くろくん」が可愛いですよ。",
                                               web_page: "https://google.com",
-                                              rating: 5 } }
+                                              rating: 5,
+                                              picure: picture2 } }
       redirect_to shop
       follow_redirect!
       expect(response).to render_template('shops/show')
