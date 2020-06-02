0a6f7304379cc1a2bb7210131b02334e4b6e2957
ecru <woovoodoor@yahoo.co.jp>
2020-06-02 14:28:02 +0900

    RSpec: 受動的フォローについて(モデル)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe User, type: :model do
       expect(user.following?(other_user)).to be_falsey
       user.follow(other_user)
       expect(user.following?(other_user)).to be_truthy
+      expect(other_user.followed_by?(user)).to be_truthy
       user.unfollow(other_user)
       expect(user.following?(other_user)).to be_falsey
     end
