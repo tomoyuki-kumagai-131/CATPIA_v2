943eafa0e5f6e520db15ecfafff4af4575a08fa4
ecru <woovoodoor@yahoo.co.jp>
2020-06-08 20:39:11 +0900

    RSpecテスト用ファクトリ作成

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :notification do
     shop_id { 1 }
-    variety 1
+    variety { 1 } 
     content ""
-    from_user_id { 1 }
+    from_user_id { 2 }
     association :user
   end
 end
