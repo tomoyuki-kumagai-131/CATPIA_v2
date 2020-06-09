81769eb5de8f6aa25da7acd278a61b2dc125f6ca
ecru <woovoodoor@yahoo.co.jp>
2020-06-09 13:45:07 +0900

    通知一覧のindexアクション作成

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,5 +2,7 @@ class NotificationsController < ApplicationController
   before_action :logged_in_user
  
   def index
+    @notifications = current_user.notifications
+    current_user.update_attribute(:notification, false)
   end
 end
