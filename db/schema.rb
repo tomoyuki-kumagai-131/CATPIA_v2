e52343fba6aa9d5c87f5e94b8a7d9b85af998275
ecru <woovoodoor@yahoo.co.jp>
2020-06-12 08:35:37 +0900

    schema追記

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2020_06_08_122432) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["shop_id"], name: "index_comments_on_shop_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
