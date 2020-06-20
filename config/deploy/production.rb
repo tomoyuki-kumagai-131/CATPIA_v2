6e3a9c63d1dd99ae8335d513e2bd6724cabb24e0
ecru <woovoodoor@yahoo.co.jp>
2020-06-21 08:07:21 +0900

    Capistrano設定

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,15 +1,9 @@
 # EC2サーバーのIP、EC2サーバーにログインするユーザー名、サーバーのロールを記述
-server '54.199.70.11', user: 'tomo', roles: %w{app db web}
-
-#デプロイするサーバーにsshログインする鍵の情報を記述
-set :ssh_options, keys: '~/.ssh/catpia_v4.pem' 
-
 server '54.199.70.11',
    user: 'tomo',
    roles: %w{web db app},
    ssh_options: {
        port: 22,
-       keys: %w(~/.ssh/catpia_v4.pem),
+       keys: '~/.ssh/catpia_v4.pem',
        forward_agent: true
       }
-      
