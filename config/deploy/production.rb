6735e5fb92daad7949eb0c6678e658509a7a3813
ecru <woovoodoor@yahoo.co.jp>
2020-06-21 08:29:01 +0900

    設定

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,4 @@
 # EC2サーバーのIP、EC2サーバーにログインするユーザー名、サーバーのロールを記述
-server '54.199.70.11',
-    user: 'ec2-user',
-    roles: %w{web db app},
-set :ssh_options: {
-        port: 22,
-        keys: '~/.ssh/catpia_v4.pem',
-        }
+server '54.199.70.11', user: 'ec2-user', roles: %w{web db app},
+
+set :ssh_options, keys: '~/.ssh/catpia_v4.pem'
