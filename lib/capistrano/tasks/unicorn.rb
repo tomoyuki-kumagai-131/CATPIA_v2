e1bc9710501b7e603c22618d213f1ca6c31c6057
ecru <woovoodoor@yahoo.co.jp>
2020-06-23 16:13:59 +0900

    Capistrano設定

--- lib/capistrano/tasks/unicorn.rb
+++ lib/capistrano/tasks/unicorn.rb
@@ -1,50 +1,50 @@
-#unicornのpidファイル、設定ファイルのディレクトリを指定
+# unicornのpidファイル、設定ファイルのディレクトリを指定
 namespace :unicorn do
   task :environment do
-    set :unicorn_pid,    "#{current_path}/tmp/pids/unicorn.pid"
+    set :unicorn_pid,    "#{shared_path}/tmp/pids/unicorn.pid"
     set :unicorn_config, "#{current_path}/config/unicorn/production.rb"
   end
 
-#unicornをスタートさせるメソッド
+  # unicornをスタートさせるメソッド
   def start_unicorn
     within current_path do
       execute :bundle, :exec, :unicorn, "-c #{fetch(:unicorn_config)} -E #{fetch(:rails_env)} -D"
     end
   end
 
-#unicornを停止させるメソッド
+  # unicornを停止させるメソッド
   def stop_unicorn
     execute :kill, "-s QUIT $(< #{fetch(:unicorn_pid)})"
   end
 
-#unicornを再起動するメソッド
+  # unicornを再起動するメソッド
   def reload_unicorn
     execute :kill, "-s USR2 $(< #{fetch(:unicorn_pid)})"
   end
 
-#unicronを強制終了するメソッド
+  # unicronを強制終了するメソッド
   def force_stop_unicorn
     execute :kill, "$(< #{fetch(:unicorn_pid)})"
   end
 
-#unicornをスタートさせるtask
-  desc "Start unicorn server"
+  # unicornをスタートさせるtask
+  desc 'Start unicorn server'
   task start: :environment do
     on roles(:app) do
       start_unicorn
     end
   end
 
-  #unicornを停止させるtask
-  desc "Stop unicorn server gracefully"
+  # unicornを停止させるtask
+  desc 'Stop unicorn server gracefully'
   task stop: :environment do
     on roles(:app) do
       stop_unicorn
     end
   end
 
-#既にunicornが起動している場合再起動を、まだの場合起動を行うtask
-  desc "Restart unicorn server gracefully"
+  # 既にunicornが起動している場合再起動を、まだの場合起動を行うtask
+  desc 'Restart unicorn server gracefully'
   task restart: :environment do
     on roles(:app) do
       if test("[ -f #{fetch(:unicorn_pid)} ]")
@@ -55,8 +55,8 @@ namespace :unicorn do
     end
   end
 
-#unicornを強制終了させるtask 
-  desc "Stop unicorn server immediately"
+  # unicornを強制終了させるtask
+  desc 'Stop unicorn server immediately'
   task force_stop: :environment do
     on roles(:app) do
       force_stop_unicorn
