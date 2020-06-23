cb89ef1c95d5055c2822e8b63a0debbc2c4e6e38
ecru <woovoodoor@yahoo.co.jp>
2020-06-23 13:42:48 +0900

    修正

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -7,7 +7,7 @@
 #リクエストを受け取るポート番号を指定。後述
   $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
 #PIDの管理ファイルディレクトリ
-  #$pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
+  $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
 #エラーログを吐き出すファイルのディレクトリ
   $std_log = File.expand_path 'log/unicorn.log', $app_dir
 
