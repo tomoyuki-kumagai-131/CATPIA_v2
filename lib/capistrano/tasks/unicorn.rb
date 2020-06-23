03b68ca1ce0902680635da4263754bd90a61a9f8
ecru <woovoodoor@yahoo.co.jp>
2020-06-24 07:16:26 +0900

    修正

--- lib/capistrano/tasks/unicorn.rb
+++ lib/capistrano/tasks/unicorn.rb
@@ -1,10 +1,3 @@
-# unicornのpidファイル、設定ファイルのディレクトリを指定
-namespace :unicorn do
-  task :environment do
-    set :unicorn_pid,    "#{shared_path}/tmp/pids/unicorn.pid"
-    set :unicorn_config, "#{current_path}/config/unicorn/production.rb"
-  end
-
   # unicornをスタートさせるメソッド
   def start_unicorn
     within current_path do
