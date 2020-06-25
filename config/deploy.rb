84220dfd262ed8abad141598d161e0c2ea88f710
ecru <woovoodoor@yahoo.co.jp>
2020-06-25 23:00:23 +0900

    内容修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,33 +1,28 @@
-# バージョンのロック
 lock "3.7.0"
 
-set :application, "CATPIA_v2
+set :application, "CATPIA_v2"
 set :repo_url, "git@github.com:tomo-0131/CATPIA_v2.git"
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :confirm do
+    on roles(:app) do
+      puts "This stage is '#{fetch(:stage)}'. Deploying branch is '#{fetch(:branch)}'."
+      puts 'Are you sure? [y/n]'
+      ask :answer, 'n'
+      if fetch(:answer) != 'y'
+        puts 'deploy stopped'
+        exit
+      end
+    end
+  end
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      invoke 'deploy'
+    end
+  end
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+  before :starting, :confirm
+end
