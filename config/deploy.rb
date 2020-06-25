05a9e5d942e3e6829b90ea822c444679cf9b524f
ecru <woovoodoor@yahoo.co.jp>
2020-06-26 00:09:09 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -78,6 +78,13 @@ namespace :deploy do
     end
   end
 
+  desc "Restart Application"
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+  
   before :starting,     :confirm
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
