1a294a638638a99a12f8009ccc11bd6f26aee4c6
ecru <woovoodoor@yahoo.co.jp>
2020-06-26 02:53:01 +0900

    ファイル修正

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,15 +1,7 @@
 class PictureUploader < CarrierWave::Uploader::Base
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader:
-   # developmentとtest以外はS3を使用
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  storage :file
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
@@ -21,15 +13,6 @@ class PictureUploader < CarrierWave::Uploader::Base
      "/images/" + [version_name, "default.png"].compact.join('_')
    end
 
-  # Process files as they are uploaded:
-  # process scale: [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
-  # Create different versions of your uploaded files:
-  
   # 画像サイズ設定
   # 詳細表示用：400 * 400の正方形に整形
   version :thumb600 do
@@ -41,8 +24,6 @@ class PictureUploader < CarrierWave::Uploader::Base
     process resize_to_fill: [400, 400, "Center"]
   end
 
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
   def extension_whitelist
     %w(jpg jpeg gif png)
   end
