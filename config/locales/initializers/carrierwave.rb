ca3497b3291ef7a12f5bb131b7d607b45fb4cacb
ecru <woovoodoor@yahoo.co.jp>
2020-06-27 10:24:36 +0900

    fix

--- config/locales/initializers/carrierwave.rb
+++ config/locales/initializers/carrierwave.rb
@@ -6,7 +6,6 @@ require 'carrierwave/storage/fog'
 # 保存先の分岐
 CarrierWave.configure do |config|
   if Rails.env.production? # 本番環境:AWS使用
-    config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
@@ -14,6 +13,7 @@ CarrierWave.configure do |config|
       aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
       region: 'ap-northeast-1' # 東京はap-northeast-1
     }
+    config.storage = :fog
     config.fog_directory  = 'catpia' #S3のバケット名
     config.asset_host = 'http://catpia.s3-website-ap-northeast-1.amazonaws.com'
   else
