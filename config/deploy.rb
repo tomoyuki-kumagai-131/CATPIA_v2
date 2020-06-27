45ce44bcf3d1fc20f6021ec0493bc5df7cb93ed4
ecru <woovoodoor@yahoo.co.jp>
2020-06-27 09:12:51 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -87,15 +87,6 @@ namespace :deploy do
     end
   end
 
-  desc 'upload master.key'
-  task :upload do
-    on roles(:app) do |host|
-      if test "[ ! -d #{shared_path}/config ]"
-        execute "mkdir -p #{shared_path}/config"
-      end
-      upload!('config/master.key', "#{shared_path}/config/master.key")
-    end
-  end
   before :starting, 'deploy:upload'
   after :finishing, 'deploy:cleanup'
   before :starting,     :confirm
