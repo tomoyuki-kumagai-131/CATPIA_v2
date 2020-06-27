3b267a32ad0623cea2d943990d04bcbcf82df4dd
ecru <woovoodoor@yahoo.co.jp>
2020-06-27 22:04:43 +0900

    S3 fix

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -12,8 +12,7 @@ class PictureUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-   def default_url(*args)
+  def default_url(*args)
      "/images/" + [version_name, "default.png"].compact.join('_')
    end
 
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,13 +5,19 @@ require 'carrierwave/storage/fog'
 
 # 保存先の分岐
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-    region: 'ap-northeast-1' # 東京はap-northeast-1
-  }
-  config.fog_directory  = 'catpia' #S3のバケット名
-end  
+  if Rails.env.production? # 本番環境:AWS使用
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      region: 'ap-northeast-1' # 東京はap-northeast-1
+    }
+    config.fog_directory  = 'catpia'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/catpia'
+  else
+    config.storage :file # 開発環境:public/uploades下に保存
+    config.enable_processing = false if Rails.env.test? #test:処理をスキップ
+  end  
+end
