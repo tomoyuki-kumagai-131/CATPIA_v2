2824a7951a730cbfa927b68fb5b8a961bbc3059b
ecru <woovoodoor@yahoo.co.jp>
2020-06-28 12:54:46 +0900

    S3 fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,21 +3,16 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-# 保存先の分岐
+# 保存先
 CarrierWave.configure do |config|
-  if Rails.env.production? # 本番環境:AWS使用
-    config.storage = :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-      region: 'ap-northeast-1' # 東京はap-northeast-1
-    }
-    config.fog_directory  = 'catpia'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/catpia'
-  else
-    config.storage :file # 開発環境:public/uploades下に保存
-    config.enable_processing = false if Rails.env.test? #test:処理をスキップ
-  end  
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+    region: 'ap-northeast-1' # 東京はap-northeast-1
+  }
+  config.fog_directory  = 'catpia'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/catpia'
 end
