c99f697b0091029d85d72808a22e182ee1ec7d9e
ecru <woovoodoor@yahoo.co.jp>
2020-06-30 01:06:49 +0900

    fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,6 +10,7 @@ CarrierWave.configure do |config|
   elsif Rails.env.production?
     config.storage = :fog
     config.fog_provider = 'fog/aws'
+    config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
