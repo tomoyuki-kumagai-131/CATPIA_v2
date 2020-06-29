b1bfc801214eaa33b01395af6f95046fd3f794b8
ecru <woovoodoor@yahoo.co.jp>
2020-06-29 16:25:29 +0900

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,8 +46,8 @@ set :linked_files, %w{ config/master.key }
 set :linked_files, %w{ config/credentials.yml.enc }
 
 set :default_env, {
-  rbenv_root: "/usr/local/bin/rbenv",
-  path: "/usr/local/bin/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 }
