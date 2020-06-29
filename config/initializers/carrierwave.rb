a7e0223b3d2d07c42c6eb7599c476970785b3b93
ecru <woovoodoor@yahoo.co.jp>
2020-06-30 00:02:44 +0900

    fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -15,6 +15,6 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1'
     }
     config.fog_directory = 'catpia'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/catpia/'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/catpia'
   end
 end
