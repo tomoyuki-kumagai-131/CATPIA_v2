fbe6aafb4f77e3e7636ddfb3512dff47334aa251
ecru <woovoodoor@yahoo.co.jp>
2020-06-29 15:23:06 +0900

    fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,14 +3,15 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
+  config.storage = :fog
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: 'ap-northeast-1'
   }
-  config.storage = :fog
-  config.fog_directory = 'catpia'
+
+  config.fog_directory  = 'catpia'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/catpia'
 end
