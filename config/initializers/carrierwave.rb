3abaa785bd5af8228263211e76f141f420a78bed
ecru <woovoodoor@yahoo.co.jp>
2020-06-29 23:58:30 +0900

    fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,19 +3,18 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test? #開発とテストは今まで通りに
+  if Rails.env.development? || Rails.env.test?
     config.storage = :file
-  elsif Rails.env.production? #本番はS3に保存する
+  elsif Rails.env.production?
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
       aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-# credentials下にaws_access_key_idとaws_secret_access_keyはあるよ
       region: 'ap-northeast-1'
     }
-    config.fog_directory  = 'catpia''
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/catpia'
+    config.fog_directory = 'catpia'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/catpia/'
   end
 end
