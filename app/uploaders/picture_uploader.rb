86bbe1f9b88f9159373a93d22b434e2dc7915d3d
ecru <woovoodoor@yahoo.co.jp>
2020-06-30 12:42:21 +0900

    fix

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,10 +1,6 @@
 class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  if Rails.env.development? || Rails.env.test? 
-    storage :file
-  else
     storage :fog
-  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
@@ -27,5 +23,9 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   def extension_whitelist
     %w(jpg jpeg gif png)
+  end
+
+  def filename
+    original_filename if original_filename
   end
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,21 +3,16 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test?
-    config.storage = :file
-  elsif Rails.env.production?
-    config.fog_provider = 'fog/aws'
-    config.fog_public = false
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region: 'ap-northeast-1'
-    }
-    config.storage = :fog
-    config.fog_directory = 'catpia'
-    config.asset_host = 'https://cat-pia.com'
-  end
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
+    aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
+    region: 'ap-northeast-1'
+  }
+  config.storage = :fog
+  config.fog_directory = 'catpia'
+  config.asset_host = 'https://s3.ap-northeast-1.amazonaws.com/catpia'
 end
 
 CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
