cfad9ae66d0371fbe282a700572f3f63e5b2fb22
ecru <woovoodoor@yahoo.co.jp>
2020-06-30 12:59:15 +0900

    fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,16 +3,16 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
   config.fog_provider = 'fog/aws'
-  config.fog_directory  = 'catpia'
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
-    aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key), 
+    aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
     region: 'ap-northeast-1'
   }
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/catpia'
+  config.storage = :fog
+  config.fog_directory = 'catpia'
+  config.asset_host = 'https://s3.ap-northeast-1.amazonaws.com/catpia'
 end
 
 CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
