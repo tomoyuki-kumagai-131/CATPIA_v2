72b1a70ce7ec51e0ad2e9d41913cf9a75678ac20
ecru <woovoodoor@yahoo.co.jp>
2020-06-30 13:21:00 +0900

    fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,16 +3,20 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
-    aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-    region: 'ap-northeast-1'
-  }
-  config.storage = :fog
-  config.fog_directory = 'catpia'
-  config.asset_host = 'https://s3.ap-northeast-1.amazonaws.com/catpia'
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  elsif Rails.env.production?
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region: 'ap-northeast-1'
+    }
+    config.storage = :fog
+    config.fog_directory = 'catpia'
+    config.asset_host = 'https://s3.ap-northeast-1.amazonaws.com/catpia'
+  end
 end
 
 CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
