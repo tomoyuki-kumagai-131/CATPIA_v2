59ebc5e3a9b97200121eaab1e6eb6f7b8267c5eb
ecru <woovoodoor@yahoo.co.jp>
2020-07-03 00:36:15 +0900

    修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,11 +7,11 @@ CarrierWave.configure do |config|
   config.fog_directory  = 'catpia' # 作成したバケット名を記述
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], # 環境変数
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # 環境変数
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"], # 環境変数
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"], # 環境変数
     region: 'ap-northeast-1',   # アジアパシフィック(東京)を選択した場合
     path_style: true
   }
   config.storage :fog
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/catpia'
+  config.asset_host = 'https://s3.ap-northeast-1.amazonaws.com/catpia'
 end 
