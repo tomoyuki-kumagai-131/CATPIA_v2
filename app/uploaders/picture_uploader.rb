9a5968c688a1a426e903d3fc1d3f24b9161cba11
ecru <woovoodoor@yahoo.co.jp>
2020-07-03 00:27:39 +0900

    AWS S3 関連修正

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,11 +1,7 @@
 class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-
-  if Rails.env.development? || Rails.env.test?
-    storage :file
-  else
+    
     storage :fog
-  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,20 +3,15 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test?
-    config.storage = :file
-  elsif Rails.env.production?
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region: 'ap-northeast-1'
-    }
-    config.storage = :fog
-    config.fog_directory = 'catpia'
-    config.asset_host = 'https://s3.ap-northeast-1.amazonaws.com/catpia'
-  end
-end
-
-CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+  config.fog_provider = 'fog/aws'
+  config.fog_directory  = 'catpia' # 作成したバケット名を記述
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], # 環境変数
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # 環境変数
+    region: 'ap-northeast-1',   # アジアパシフィック(東京)を選択した場合
+    path_style: true
+  }
+  config.storage :fog
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/catpia'
+end 
