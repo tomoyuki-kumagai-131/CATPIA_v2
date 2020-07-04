5cbcf71e08049f472b4e00d534c14872b495392a
ecru <woovoodoor@yahoo.co.jp>
2020-07-04 18:30:09 +0900

    修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,8 @@
 server "54.199.70.11", user: "tomo", roles: %w{app db web}
 
 set :ssh_options, {
+  keys: %w(~/.ssh/catpia_v4.pem),
   forward_agent: true,
   auth_methods: %w(publickey),
-  keys: [File.expand_path('~/catpia_v4.pem)')],
   port: 22
 }
