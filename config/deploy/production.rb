a612c78a8f5a4cfcb33223096c549152a1bd8878
ecru <woovoodoor@yahoo.co.jp>
2020-07-04 23:10:52 +0900

    インデント修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@ server "54.199.70.11",
   user: "tomo",
   roles: %w{app db web},
   port: 22,
-  ssh_options, {
+  ssh_options {
     user: "tomo",
     keys: %w(~/.ssh/catpia_v4.pem),
     #auth_methods: %w(publickey),
