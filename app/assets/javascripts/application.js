69a6ba8160921dd72179832365033acab06c610f
ecru <woovoodoor@yahoo.co.jp>
2020-07-07 11:46:59 +0900

    コード位置修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
-//= require jquery3
 //= require popper
 //= require bootstrap-sprockets
 //= require bootstrap
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-
