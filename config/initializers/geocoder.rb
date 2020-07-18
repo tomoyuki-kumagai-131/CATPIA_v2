f62dc2d411ded5dc1dae478ef507086d46ffe2ed
ecru <woovoodoor@yahoo.co.jp>
2020-07-18 17:13:15 +0900

    コード追記

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -7,6 +7,7 @@ Geocoder.configure(
 
   # IP address geocoding service (default :ipinfo_io)
   # ip_lookup: :maxmind,
+  use_https: true,
 
   # to use an API key:
   api_key: 'AIzaSyDzGSDEEbmPELGDc93uGJ2yBw8zSWtMEmc',
