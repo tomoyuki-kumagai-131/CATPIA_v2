efa348972fbb4a66c80708b18f60dad17fa9627a
ecru <woovoodoor@yahoo.co.jp>
2020-07-18 16:21:16 +0900

    修正

---
+++ config/initializers/geocoder.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+Geocoder.configure(
+
+  # street address geocoding service (default :nominatim)
+  lookup: :google,
+
+  # IP address geocoding service (default :ipinfo_io)
+  # ip_lookup: :maxmind,
+
+  # to use an API key:
+  api_key: 'AIzaSyDzGSDEEbmPELGDc93uGJ2yBw8zSWtMEmc',
+
+  # geocoding service request timeout, in seconds (default 3):
+  timeout: 5,
+
+  # set default units to kilometers:
+  units: :km
+)
