02d8f8fbd9202cd846dcc9622fbae054990ce43a
ecru <woovoodoor@yahoo.co.jp>
2020-10-05 14:30:48 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,8 +42,8 @@ set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
 set :keep_releases, 2
-#set :linked_files, %w{ config/master.key }
-set :linked_files, %w{ config/credentials.yml.enc }
+set :linked_files, %w{ config/master.key }
+#set :linked_files, %w{ config/credentials.yml.enc }
 
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
