a9503023ac80b9ac8e5ea5956306f4bb2a1eed6d
ecru <woovoodoor@yahoo.co.jp>
2020-10-05 09:22:25 +0900

    pem修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/catpia_v4.pem), port: 22 }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/catpiav6.pem), port: 22 }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
