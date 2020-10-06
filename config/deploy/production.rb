6c56d12d2a8228e693dac2e90644d9d67a55a7c3
ecru <woovoodoor@yahoo.co.jp>
2020-10-06 16:22:40 +0900

    rsa修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 server "54.199.70.11", user: "tomo", roles: %w{app db web}
 
 set :ssh_options, {
-  keys: %w(~/.ssh/catpiav6.pem),
+  keys: %w(~/.ssh/catpiav6_key_rsa),
   forward_agent: true,
   auth_methods: %w(publickey),
   port: 22
