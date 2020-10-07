b03f923d8ddd112bfe140f33d1f3ba5e24e25d83
ecru <woovoodoor@yahoo.co.jp>
2020-10-07 17:01:02 +0900

    api_key修正

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -10,7 +10,7 @@ Geocoder.configure(
   use_https: true,
 
   # to use an API key:
-  api_key: 'AIzaSyDzGSDEEbmPELGDc93uGJ2yBw8zSWtMEmc',
+  api_key: 'AIzaSyAh_mxXXCr5aAy-KrOHfg_q3SZsxNiwHPg',
 
   # geocoding service request timeout, in seconds (default 3):
   timeout: 5,
