e692b8188f05eea1a870118aa340e4593b818006
ecru <woovoodoor@yahoo.co.jp>
2020-10-08 17:22:36 +0900

    puma.sock修正

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,5 @@
 app_dir = File.expand_path("../../", __FILE__)
-bind "unix://#{app_dir}/tmp/sockets/CATPIA_v2-puma.sock"
+bind "unix://#{app_dir}/tmp/sockets/puma.sock"
 pidfile "#{app_dir}/tmp/pids/puma.pid"
 state_path "#{app_dir}/tmp/pids/puma.state"
 stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
