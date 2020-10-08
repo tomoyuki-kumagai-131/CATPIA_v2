3d314397d0f3bba53840ef5bc4803f351497a0f6
ecru <woovoodoor@yahoo.co.jp>
2020-10-08 17:51:40 +0900

    修正

--- config/puma.rb
+++ config/puma.rb
@@ -7,6 +7,6 @@ stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.lo
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
-#port        ENV.fetch("PORT") { 3000 }
+port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 plugin :tmp_restart
