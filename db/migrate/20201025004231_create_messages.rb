5cb9cf0b020f90d774fa0ed57eec2027b31a968f
ecru <woovoodoor@yahoo.co.jp>
2020-10-25 23:50:50 +0900

    Messageモデル作成

---
+++ db/migrate/20201025004231_create_messages.rb
@@ -0,0 +1,11 @@
+class CreateMessages < ActiveRecord::Migration[5.2]
+  def change
+    create_table :messages do |t|
+      t.references :user, foreign_key: true
+      t.references :room, foreign_key: true
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
