e7413832c8d2b6da74627856e4391e70aca9ee47
ecru <woovoodoor@yahoo.co.jp>
2020-10-26 01:17:19 +0900

    destroyメソッドの追記(DM)

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,22 @@
 class MessagesController < ApplicationController
-  before_action :logged_in_user, :only => [:create]
+    before_action :logged_in_user #, :only => [:create]
 
-  def create
-    if Entry.where(:user_id => current_user.id, :room_id => params[:message][:room_id]).present?
-      @message = Message.create(params.require(:message).permit(:user_id, :content, :room_id).merge(:user_id => current_user.id))
-      redirect_to "/rooms/#{@message.room_id}"
-    else
-      redirect_back(fallback_location: root_path)
+    def create
+      if Entry.where(:user_id => current_user.id, :room_id => params[:message][:room_id]).present?
+        @message = Message.create(params.require(:message).permit(:user_id, :content, :room_id).merge(:user_id => current_user.id))
+        redirect_to "/rooms/#{@message.room_id}"
+      else
+        redirect_back(fallback_location: root_path)
+      end
     end
-  end
+
+    def destroy
+      @message = Message.find(params[:id])
+      # @shop = @comment.shop
+      if current_user.id == @message.user_id
+        @message.destroy
+        flash[:success] = "メッセージを削除しました！"
+      end
+      redirect_to "/rooms/#{@message.room_id}"
+    end
 end
