9563dfd7a6e4a7e88bbf0bc0ba4a3f0964d6731e
ecru <woovoodoor@yahoo.co.jp>
2020-10-25 23:53:03 +0900

    Entryモデル作成

---
+++ app/models/entry.rb
@@ -0,0 +1,5 @@
+class Entry < ApplicationRecord
+  belongs_to :user
+  belongs_to :room
+  #validates :room_id, uniqueness: { scope: :user_id }
+end
---
+++ spec/factories/entries.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :entry do
+    user nil
+    room nil
+  end
+end
