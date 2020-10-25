fd36c9e999d5eb059178e82907ad8948740aeb94
ecru <woovoodoor@yahoo.co.jp>
2020-10-25 23:53:34 +0900

    usersコントローラ追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,24 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @shops = Shop.page(params[:page]).per(10)
+    @currentUserEntry = Entry.where(user_id: current_user.id)
+    @userEntry = Entry.where(user_id: @user.id)
+
+    unless @user.id == current_user.id
+      @currentUserEntry.each do |cu|
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id then
+            @isRoom = true
+            @roomId = cu.room_id
+          end
+        end
+      end
+      #無ければ作る
+      unless @isRoom
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
   end
 
   def create
