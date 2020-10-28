169fc9c16ad0606aafa0556aa5831ab87ea29480
ecru <woovoodoor@yahoo.co.jp>
2020-10-29 01:31:39 +0900

    ログイン後遷移をフィード画面に変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       log_in user
       flash[:success] = "ログインしました！"
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
+      redirect_to root_url
     else
       flash[:error] = "メールアドレスとパスワードの組み合わせが誤っています"
       render 'new'
