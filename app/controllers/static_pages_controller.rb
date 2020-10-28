8fd952233e829fa779274acdf706f6da9447ad62
ecru <woovoodoor@yahoo.co.jp>
2020-10-29 01:32:12 +0900

    フィード画面枚数を5から6に変更

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   # @feed_items ...feedメソッドの各要素はShopクラスを持つ
   def home
     if logged_in?
-      @feed_items = current_user.feed.page(params[:page]).per(5)
+      @feed_items = current_user.feed.page(params[:page]).per(6)
     end
   end
 
