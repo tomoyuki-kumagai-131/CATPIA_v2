a9348616c742257fa6fcda178f43d60c3461c2d8
ecru <woovoodoor@yahoo.co.jp>
2020-11-17 14:09:51 +0900

    修正

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module AppName
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.autoloader = :classic
     config.generators do |g|
       g.test_framework :rspec,
         controller_specs: false,
